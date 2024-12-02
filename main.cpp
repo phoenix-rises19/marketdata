5261bc1315b98d7e6e3a3b495597529df53b080e
phoenix-rises19 <manocha.vaibhav19@gmail.com>
2024-12-02 18:31:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,226 +12,233 @@
 #include<climits>
 #include <cstdint>
 #include <fstream>
-#pragma pack(push, 1)
+#pragma pack(1)
 
 struct header {
     uint64_t rxTimestamp_;
     int16_t msg_len_;
     int16_t streamId_;
-    int32_t seqno_;
+    int32_t seqNo_;
     char msgType_;
 
     void display() const {
-        std::cout << rxTimestamp_ << "\t" << msg_len_ << "\t" << streamId_ << "\t" << seqno_ << "\t" << msgType_ << std::endl;
+        std::cout << rxTimestamp_ << "\t" << msg_len_ << "\t" << streamId_ << "\t" << seqNo_ << "\t" << msgType_ <<
+                std::endl;
     }
 };
 
-#pragma pack(pop)
-
-class Order {
+class orderMsg {
 public:
-    uint64_t rxTimestamp_;
-    short streamId_;
-    short seqNumber_;
     char msgType_;
-    long timestamp_;
-    double orderId_;
-    int token_;
+    uint64_t timestamp_;
+    uint64_t orderId_;
+    uint32_t token_;
     char orderType_;
-    int price_;
-    int quantity_;
+    uint32_t price_;
+    uint32_t quantity_;
 
+    void getMessage(orderMsg &order, const char *buffer) {
+        order.msgType_ = buffer[0];
 
-    Order(long ts, short streamId_, short seqNumber_, char msgtyp, long times, double id, int token, char ordertype,
-          int price, int quantity)
-        : rxTimestamp_(ts), streamId_(streamId_), seqNumber_(seqNumber_), msgType_(msgtyp), timestamp_(times),
-          orderId_(id), token_(token), orderType_(ordertype), price_(price), quantity_(quantity) {
+        // Reverse the bytes (convert from little-endian to big-endian)
+        unsigned char reversed_bytes[8];
+        for (int i = 0; i < 8; ++i) {
+            reversed_bytes[i] = buffer[8 - i];
+        }
+
+        // Combine the reversed bytes into an integer
+        order.timestamp_ = 0;
+        for (int i = 0; i < 8; ++i) {
+            timestamp_ |= (static_cast<uint64_t>(reversed_bytes[i]) << (8 * (7 - i)));
+        }
+
+        for (int i = 0; i < 8; ++i) {
+            reversed_bytes[i] = buffer[16 - i];
+        }
+        order.orderId_ = 0;
+        for (int i = 0; i < 8; ++i) {
+            orderId_ |= (static_cast<uint64_t>(reversed_bytes[i]) << (8 * (7 - i)));
+        }
+
+        unsigned char reversed_bytes2[4];
+        for (int i = 0; i < 4; ++i) {
+            reversed_bytes2[i] = buffer[20 - i];
+        }
+        order.token_ = 0;
+        for (int i = 0; i < 4; ++i) {
+            token_ |= (static_cast<uint32_t>(reversed_bytes2[i]) << (8 * (3 - i)));
+        }
+        order.orderType_ = buffer[21];
+
+        for (int i = 0; i < 4; ++i) {
+            reversed_bytes2[i] = buffer[25 - i];
+        }
+        order.price_ = 0;
+        for (int i = 0; i < 4; ++i) {
+            price_ |= (static_cast<uint32_t>(reversed_bytes2[i]) << (8 * (3 - i)));
+        }
+        price_ = price_ / 100;
+
+        for (int i = 0; i < 4; ++i) {
+            reversed_bytes2[i] = buffer[29 - i];
+        }
+        order.quantity_ = 0;
+        for (int i = 0; i < 4; ++i) {
+            quantity_ |= (static_cast<uint32_t>(reversed_bytes2[i]) << (8 * (3 - i)));
+        }
     }
 
-
     void display() const {
-        std::cout << rxTimestamp_ << "\t" << timestamp_ << "\t" << orderId_ << "\t" << token_ << "\t" << price_ << "\t"
+        std::cout << msgType_ << "\t" << timestamp_ << "\t" << orderId_ << "\t" << token_ << "\t" << orderType_ << "\t"
+                << price_ << "\t"
                 << quantity_ << std::endl;
     }
+};
 
+class tradeMsg {
+public:
+    char msgType_;
+    uint64_t timestamp_;
+    uint64_t buyOrderId_;
+    uint64_t sellOrderId_;
+    uint32_t token_;
+    uint32_t tradePrice_;
+    uint32_t quantity_;
 
-    bool operator<(const Order &other) const {
-        if (price_ == other.price_) {
-            return timestamp_ < other.timestamp_;
+    void getMessage(tradeMsg &trade, const char *buffer) {
+        msgType_ = buffer[0];
+        // Reverse the bytes (convert from little-endian to big-endian)
+        unsigned char reversed_bytes[8];
+        for (int i = 0; i < 8; ++i) {
+            reversed_bytes[i] = buffer[8 - i];
         }
-        return price_ > other.price_;
+        // Combine the reversed bytes into an integer
+        timestamp_ = 0;
+        for (int i = 0; i < 8; ++i) {
+            timestamp_ |= (static_cast<uint64_t>(reversed_bytes[i]) << (8 * (7 - i)));
+        }
+
+        for (int i = 0; i < 8; ++i) {
+            reversed_bytes[i] = buffer[16 - i];
+        }
+        buyOrderId_ = 0;
+        for (int i = 0; i < 8; ++i) {
+            buyOrderId_ |= (static_cast<uint64_t>(reversed_bytes[i]) << (8 * (7 - i)));
+        }
+
+        for (int i = 0; i < 8; ++i) {
+            reversed_bytes[i] = buffer[24 - i];
+        }
+        sellOrderId_ = 0;
+        for (int i = 0; i < 8; ++i) {
+            sellOrderId_ |= (static_cast<uint64_t>(reversed_bytes[i]) << (8 * (7 - i)));
+        }
+
+        unsigned char reversed_bytes2[4];
+        for (int i = 0; i < 4; ++i) {
+            reversed_bytes2[i] = buffer[28 - i];
+        }
+        token_ = 0;
+        for (int i = 0; i < 4; ++i) {
+            token_ |= (static_cast<uint32_t>(reversed_bytes2[i]) << (8 * (3 - i)));
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            reversed_bytes2[i] = buffer[32 - i];
+        }
+        tradePrice_ = 0;
+        for (int i = 0; i < 4; ++i) {
+            tradePrice_ |= (static_cast<uint32_t>(reversed_bytes2[i]) << (8 * (3 - i)));
+        }
+        tradePrice_ = tradePrice_ / 100;
+
+        for (int i = 0; i < 4; ++i) {
+            reversed_bytes2[i] = buffer[36 - i];
+        }
+        quantity_ = 0;
+        for (int i = 0; i < 4; ++i) {
+            quantity_ |= (static_cast<uint32_t>(reversed_bytes2[i]) << (8 * (3 - i)));
+        }
     }
 
-
-    bool operator>(const Order &other) const {
-        if (price_ == other.price_) {
-            return timestamp_ > other.timestamp_;
-        }
-        return price_ < other.price_;
+    void display() const {
+        std::cout << msgType_ << "\t" << timestamp_ << "\t" << buyOrderId_ << "\t" << sellOrderId_ << "\t" << token_ <<
+                "\t" << tradePrice_ << "\t"
+                << quantity_ << std::endl;
     }
 };
 
-class BidTable {
-private:
-    // std::map where key = price and value = Order
-    std::map<double, Order, std::greater<> > bids;
+class OrderBook {
+    static bool BidCompare(const orderMsg &a, const orderMsg &b) {
+        if (a.price_ != b.price_) {
+            return a.price_ > b.price_;
+        }
+        return a.timestamp_ < b.timestamp_;
+    }
+
+    static bool AskCompare(const orderMsg &a, const orderMsg &b) {
+        if (a.price_ != b.price_) {
+            return a.price_ < b.price_;
+        }
+        return a.timestamp_ < b.timestamp_;
+    }
+
+    std::multimap<u_int32_t, orderMsg, decltype(&OrderBook::BidCompare)> BidTable;
+    std::multimap<u_int32_t, orderMsg, decltype(&OrderBook::AskCompare)> AskTable;
 
 public:
-    // Add a new bid order
-    void addOrder(long ts, short streamId_, short seqNumber_, char msgtyp, long times, double id, int token,
-                  char ordertype, int price, int quantity) {
-       // bids[price] = Order(ts, streamId_, seqNumber_, msgtyp, times, id, token, ordertype, price, quantity);
-        display();
+    OrderBook()
+        : BidTable(&OrderBook::BidCompare), AskTable(&OrderBook::AskCompare) {
     }
-
-    // Delete an order by order_id
-    void deleteOrder(int order_id) {
-        for (auto it = bids.begin(); it != bids.end(); ++it) {
-            if (it->second.orderId_ == order_id) {
-                bids.erase(it);
-                break;
+    void addOrder(const orderMsg &order) {
+        if (order.orderType_ == 'B') {
+            if (order.msgType_=='N') {
+                BidTable.insert(std::make_pair(order.price_, order));
             }
+            else if (order.msgType_=='M') {
+             
+            }
+        } else {
+            AskTable.insert(std::make_pair(order.price_, order));
         }
-        display();
     }
-
-    // Modify an existing order
-    void modifyOrder(long ts, short streamId_, short seqNumber_, char msgtyp, long times, double id, int token,
-                     char ordertype, int price, int quantity) {
-        deleteOrder(id);
-        addOrder(ts, streamId_, seqNumber_, msgtyp, times, id, token, ordertype, price, quantity);
-    }
-
-    // Display the bid table (sorted by price descending)
     void display() const {
-        std::cout << "Bid Table (Sorted by Price Descending):\n";
-        std::cout << "RxTimestamp\tStreamID\tSeqNumber\tTimestamp\tOrderID\tToken\tOrderType\tPrice\tQuantity\n";
-        for (const auto &order: bids) {
-            order.second.display();
-        }
-        std::cout << "------------------------\n";
+        std::cout << "Bid Table" << std::endl;
     }
 };
 
-
-class AskTable {
-private:
-    // std::map where key = price and value = Order
-    std::map<double, Order> asks;
-
-public:
-    // Add a new ask order
-    void addOrder(long ts, short streamId_, short seqNumber_, char msgtyp, long times, double id, int token,
-                  char ordertype, int price, int quantity) {
-        //checks for add?
-       // asks[price] = Order(ts, streamId_, seqNumber_, msgtyp, times, id, token, ordertype, price, quantity);
-        display();
-    }
-
-    // Delete an order by order_id
-    void deleteOrder(int order_id) {
-        for (auto it = asks.begin(); it != asks.end(); ++it) {
-            if (it->second.orderId_ == order_id) {
-                asks.erase(it);
-                break;
-            }
-        }
-        display();
-    }
-
-    // Modify an existing order
-    void modifyOrder(long ts, short streamId_, short seqNumber_, char msgtyp, long times, double id, int token,
-                     char ordertype, int price, int quantity) {
-        deleteOrder(id); // Remove the old order
-        addOrder(ts, streamId_, seqNumber_, msgtyp, times, id, token, ordertype, price, quantity);
-        // Add the modified order
-    }
-
-    // Display the ask table (sorted by price ascending)
-    void display() const {
-        std::cout << "Ask Table (Sorted by Price Ascending):\n";
-        std::cout << "RxTimestamp\tStreamID\tSeqNumber\tTimestamp\tOrderID\tToken\tOrderType\tPrice\tQuantity\n";
-        for (const auto &order: asks) {
-            order.second.display();
-        }
-        std::cout << "------------------------\n";
-    }
-};
-
-void processMessage(BidTable &bid_table, AskTable &ask_table, std::pmr::vector<char> &message) {
-    int index = 0;
-    short streamId_ = static_cast<short>(static_cast<uint8_t>(message[index])) |
-                      (static_cast<short>(static_cast<uint8_t>(message[index + 1])) << 8);
-    index += 2;
-    int seqNumber_ = static_cast<int>(static_cast<uint8_t>(message[index])) |
-                     (static_cast<int>(static_cast<uint8_t>(message[index + 1])) << 8) |
-                     (static_cast<int>(static_cast<uint8_t>(message[index + 2])) << 16) |
-                     (static_cast<int>(static_cast<uint8_t>(message[index + 3])) << 24);
-    index += 4;
-    char msgType_ = static_cast<char>(static_cast<uint8_t>(message[index]));
-    index += 1;
-
-}
 
 int main() {
     //std::cout<< sizeof(header)<< std::endl;
     std::ifstream file("/home/vaibhav/cm_stream_3", std::ios::binary);
     header msgHeader;
+
     char buffer[100];
-    int count=100;
-    while (true and count-->0 ) {
-        file.read(reinterpret_cast<char*>(&msgHeader),sizeof(header));
-        if (file.gcount() !=sizeof(header)) {
+    int count = 100;
+    while (true and count-- > 0) {
+        file.read(reinterpret_cast<char *>(&msgHeader), sizeof(header));
+        if (file.gcount() != sizeof(header)) {
             std::cerr << "Error reading header!" << std::endl;
             break;
         }
         msgHeader.display();
-        file.read(buffer,msgHeader.msg_len_-9);
-        if (file.gcount() !=msgHeader.msg_len_-9) {
+        file.seekg(-1, std::ios::cur);
+        file.read(buffer, msgHeader.msg_len_ - 8);
+        if (file.gcount() != msgHeader.msg_len_ - 8) {
             std::cerr << "Error reading message data!" << std::endl;
             break;
         }
-    }
 
-    return 0;
-    BidTable bidTable;
-    AskTable askTable;
-    // multiple
-
-
-
-
-    if (!file) {
-        std::cerr << "Error opening file!" << std::endl;
-        return 0;
-    }
-
-
-    while (true) {
-        uint8_t lengthBytes[2];
-        file.read(reinterpret_cast<char *>(&lengthBytes), sizeof(lengthBytes));
-
-        // Check if we reached the end of the file
-        if (file.eof()) {
-            break;
+        if (msgHeader.msg_len_ == 38) {
+            orderMsg order;
+            order.getMessage(order, buffer);
+            order.display();
         }
-
-        // Convert the 2 bytes to a short value (little-endian format)
-        uint16_t messageLength = static_cast<uint16_t>(lengthBytes[0]) | (static_cast<uint16_t>(lengthBytes[1]) << 8);
-
-        file.seekg(2, std::ios::cur);
-        messageLength -= 2;
-        // Read the actual message based on the length
-        std::vector<char> message(messageLength);
-        file.read(message.data(), messageLength);
-
-        // Check for read errors
-        if (file.fail()) {
-            std::cerr << "Error reading message data!" << std::endl;
-            break;
+        if (msgHeader.msg_len_ == 45) {
+            tradeMsg trade;
+            trade.getMessage(trade, buffer);
+            trade.display();
         }
-       // processMessage(bidTable, askTable, message);
     }
-
     return 0;
 }
